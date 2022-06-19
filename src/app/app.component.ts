ab398e28692950790db41199c09e062bbb86cda9
Hoang Long <longnguyen20011123@gmail.com>
2022-06-19 23:21:57 +0700

    Admin front end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.onLoad();
+  }
+  constructor(private http: HttpClient) {}
+
+  onLoad() {
+    this.http
+      .get<any>('http://localhost:8070/api/sp1/product/list')
+      .subscribe((response) => {
+        console.log('xxxxxxx', response);
+      });
+  }
   title = 'AdminFE';
 }
